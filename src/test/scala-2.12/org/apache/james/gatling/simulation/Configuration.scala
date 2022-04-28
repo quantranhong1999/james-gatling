6c253fff246850e49b39ba2fac4c5a9abad4835c
Quan Tran <hqtran@linagora.com>
2022-04-28 12:14:17 +0700

    Config preprod

--- src/test/scala-2.12/org/apache/james/gatling/simulation/Configuration.scala
+++ src/test/scala-2.12/org/apache/james/gatling/simulation/Configuration.scala
@@ -7,30 +7,30 @@ import scala.util.Properties
 
 object Configuration {
 
-  val ServerHostName = Properties.envOrElse("TARGET_HOSTNAME", "localhost")
-  val JmapServerHostName = Properties.envOrElse("JMAP_TARGET_HOSTNAME", ServerHostName)
-  val ImapServerHostName = Properties.envOrElse("IMAP_SERVER_HOSTNAME", ServerHostName)
+  val ServerHostName = Properties.envOrElse("TARGET_HOSTNAME", "james-admin.upn.integration-open-paas.org")
+  val JmapServerHostName = "jmap.upn.integration-open-paas.org"
+  val ImapServerHostName = "imap.upn.integration-open-paas.org"
   val WebadminServerHostName = Properties.envOrElse("WEBADMIN_SERVER_HOSTNAME", ServerHostName)
 
-  val JMAP_PORT = Properties.envOrElse("JMAP_PORT", "1080").toInt
-  val JMAP_PROTOCOL = Properties.envOrElse("JMAP_PROTOCOL", "http")
-  val WS_PROTOCOL = Properties.envOrElse("WS_PROTOCOL", "ws")
+  val JMAP_PORT = Properties.envOrElse("JMAP_PORT", "443").toInt
+  val JMAP_PROTOCOL = Properties.envOrElse("JMAP_PROTOCOL", "https")
+  val WS_PROTOCOL = Properties.envOrElse("WS_PROTOCOL", "wss")
   val WS_PORT = Properties.envOrElse("WS_PORT", String.valueOf(JMAP_PORT)).toInt
   val BaseJmapUrl = new URL(s"$JMAP_PROTOCOL://$JmapServerHostName:$JMAP_PORT")
   val BaseWsUrl = s"$WS_PROTOCOL://$JmapServerHostName:$WS_PORT"
 
   val WEBADMIN_PORT = Properties.envOrElse("WEBADMIN_PORT", "8000").toInt
   val WEBADMIN_PROTOCOL = Properties.envOrElse("WEBADMIN_PROTOCOL", "http")
-  val BaseJamesWebAdministrationUrl = new URL(s"$WEBADMIN_PROTOCOL://$WebadminServerHostName:$WEBADMIN_PORT")
+  val BaseJamesWebAdministrationUrl = new URL(s"$WEBADMIN_PROTOCOL://$WebadminServerHostName")
 
   val DURATION_PROPERTY = Properties.envOrNone("DURATION") match {
     case Some(duration) => Some(duration.toInt minutes)
     case _ => None
   }
 
-  val ScenarioDuration = DURATION_PROPERTY.getOrElse(1 hour)
-  val InjectionDuration = DURATION_PROPERTY.getOrElse(1 hour)
-  val UserCount = 100
+  val ScenarioDuration = DURATION_PROPERTY.getOrElse(12 minutes)
+  val InjectionDuration = DURATION_PROPERTY.getOrElse(8 minutes)
+  val UserCount = 1000
   val RandomlySentMails = 10
   val NumberOfMailboxes = 10
   val NumberOfMessages = 20
